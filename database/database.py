bd4021c475b1c519b680aaea73d12179cc0de0a9
sebastian-martinez-py <smartinezabar@gmail.com>
2026-03-23 01:22:57 -0600

    migrar a postgresql

--- database/database.py
+++ database/database.py
@@ -1,9 +1,20 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
+from dotenv import load_dotenv
 
-DATABASE_URL = "sqlite:///./finanzas.db"
+load_dotenv()
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "sqlite:///./finanzas.db"
+)
+
+# Render usa postgres:// pero SQLAlchemy necesita postgresql://
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 class Base(DeclarativeBase):
